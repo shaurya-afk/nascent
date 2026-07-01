809757358959a305749282ec7985e552f72983f1
shaurya-afk <shauryakumarsharma0007@gmail.com>
2026-07-01 14:03:35 +0530

    updated: database config

--- app/core/database.py
+++ app/core/database.py
@@ -6,7 +6,10 @@ from app.core.config import settings
 engine = create_async_engine(
     settings.database_url,
     echo=False,
-    connect_args = {}
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
 )
 
 AsyncSessionLocal = async_sessionmaker(
